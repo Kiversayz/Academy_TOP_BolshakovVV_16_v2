ebbb8e91b3cc43a4470777d4b7edfa495654589d
Kiversayz <juzama12@gmail.com>
2025-10-06 11:38:02 +0300

    Добавлен шаблон выхода из авторизации

--- AcademyTop/urls.py
+++ AcademyTop/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path('user/', include('user.urls')),
     # HTML-авторизация
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 ]
 
 if settings.DEBUG:
